5b001a5ebda5c4aafbcdf663ef1ac8fad5408f9a
codingonpixelbook <codingonpixelbook@gmail.com>
2022-10-21 17:01:09 +0800

    First version

---
+++ main.py
@@ -0,0 +1,12 @@
+greeting = "Hello World"
+my_name = "Matt"
+
+print(greeting)
+print("My name is " + my_name)
+your_name = str(input("What is your name? "))
+print("Hi " + your_name)
+print("Let's do a simple multiplication")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Now enter a second number: :"))
+sum = str(num1 * num2)
+print("The answer is " + sum)
