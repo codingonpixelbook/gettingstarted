dd3b51933f6cd0b10ca3c347e207a453717daca2
codingonpixelbook <codingonpixelbook@gmail.com>
2022-10-28 17:12:40 +0800

    Added if statement

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ print(greeting)
 print("My name is " + my_name)
 your_name = str(input("What is your name? "))
 print("Hi " + your_name)
+
+if your_name == "Adam":
+    print("Your ID number is 001")
+elif your_name == "Eve":
+    print("Your ID number is 002")
+else:
+    print("You have no valid ID")
+
 print("Let's do a simple multiplication")
 num1 = float(input("Enter a number: "))
 num2 = float(input("Now enter a second number: :"))
 sum = str(num1 * num2)
 print("The answer is " + sum)
-print("Thank you")
+print("Thank you " + your_name)
